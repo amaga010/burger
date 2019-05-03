5b19b33dae951b7c55516c9f307dee041fdd6729
Rihanna <alexmagana@Alexs-Air.frontierlocal.net>
2019-05-03 11:15:34 -0700

    first

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+  VALUES
+    ("Cheeseburger", 0);
+INSERT INTO burgers
+    (burger_name, devoured)
+  VALUES
+    ("Veggie Burger", 0);
+INSERT INTO burgers
+    (burger_name, devoured)
+  VALUES
+    ("Turkey Burger", 0);
